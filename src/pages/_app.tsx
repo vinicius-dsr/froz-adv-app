ac56f6229214bb74ed9a18650360ac2f6735ae90
Vinicius <vinicius@froz-adv.com>
2026-07-21 22:48:22 -0300

    feat(fonts): apply DM Sans globally via CSS variables in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { dmSans, dmSerifDisplay } from "@/styles/fonts";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${dmSans.variable} ${dmSerifDisplay.variable}`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 }
